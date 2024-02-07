421bb2b35045f4d87c425197954541a1a6381961
Allahverdi <isaevallahverdi@gmail.com>
2024-02-08 02:41:27 +0400

    fix

--- components/Logo/Logo.tsx
+++ components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import "@/styles/components/_logo.scss";
 const Logo = () => {
   return (
     <div className="logo">
-      <Link href="/" rel="preload" as={"fetch"}>
+      <Link rel="preload" href="/" as={"fetch"}>
         <Image
           alt="createLogo"
           src={"/images/logo.png"}
