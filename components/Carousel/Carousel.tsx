a1aa1b6b15ba2c69debed1196d85e9c1b660e91b
Allahverdi <isaevallahverdi@gmail.com>
2024-02-08 00:38:29 +0400

    changes

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "@/styles/components/_carousel.scss";
-import { Pagination, Autoplay, Navigation } from "swiper/modules";
-import Image from "next/image";
-import { client, urlFor } from "@/app/lib/sanity";
+import { client } from "@/app/lib/sanity";
 import { groq } from "next-sanity";
-import { v4 } from "uuid";
 import { IImages } from "@/types/ProductTypes";
 import CarouselSwiper from "./CarouselSwiper";
 
