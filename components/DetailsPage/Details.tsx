92eefb165ad2d0b4378c6f352964af3803eeb7a3
Allahverdi <isaevallahverdi@gmail.com>
2024-02-23 14:01:40 +0400

    features/details

--- components/DetailsPage/Details.tsx
+++ components/DetailsPage/Details.tsx
@@ -3,7 +3,7 @@
 import { urlFor } from "@/app/lib/sanity";
 import { IProduct } from "@/types/ProductTypes";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "@/styles/components/_details.scss";
 import { v4 } from "uuid";
 
@@ -13,28 +13,66 @@ interface IProductProps {
 
 const Details = ({ products }: IProductProps) => {
   const [seletedItem, setSelectedItem] = useState<number>(0);
+  const outsideRef = useRef<HTMLImageElement>(null);
+
+  useEffect(() => {
+    const popupImage = document.getElementById("popup-image") as HTMLDivElement;
+    const main = document.querySelector(".main img") as HTMLImageElement;
+
+    main.addEventListener("click", () => {
+      popupImage.style.display = "block";
+      document.body.style.overflow = "hidden";
+    });
+
+    function handleClickOutside(e: Event) {
+      const target = e?.target as Element;
+      if (!outsideRef.current?.contains(target as Element)) {
+        popupImage.style.display = "none";
+        document.body.style.overflow = "scroll";
+      }
+    }
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  });
 
   return (
     <>
       <div className="main">
-        <div>
-          {products?.image[0] ? (
-            <Image
-              src={urlFor(products?.image[seletedItem]!).url()!}
-              alt={"shoes"}
-              width={300}
-              height={0}
-              unoptimized={true}
-              style={{
-                height: "480px",
-                width: "700px",
-                objectFit: "contain",
-              }}
-            />
-          ) : (
-            <></>
-          )}
-        </div>
+        {products?.image[0] ? (
+          <Image
+            src={urlFor(products?.image[seletedItem]!).url()!}
+            alt={"shoes"}
+            width={300}
+            height={0}
+            unoptimized={true}
+            ref={outsideRef}
+            style={{
+              height: "480px",
+              width: "700px",
+              objectFit: "contain",
+            }}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
+
+      <div id="popup-image">
+        <Image
+          src={urlFor(products?.image[seletedItem]!).url()!}
+          alt={"shoes"}
+          width={300}
+          height={0}
+          unoptimized={true}
+          style={{
+            height: "480px",
+            width: "700px",
+            objectFit: "contain",
+          }}
+        />
       </div>
 
       <div className="thumbs">
